7044b85e916670a4d4f269e6da13f1d32faba71b
mohamedRamadan12045 <107187838+mohamedRamadan12045@users.noreply.github.com>
2022-06-09 14:36:21 +0200

    Update contact.js
    Signed-off-by: mohamedRamadan12045 <107187838+mohamedRamadan12045@users.noreply.github.com>

--- mail/contact.js
+++ mail/contact.js
@@ -1,71 +1,65 @@
-$(function() {
+$(function () {
 
-    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
+        submitError: function ($form, event, errors) {
         },
-        submitSuccess: function($form, event) {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
+        submitSuccess: function ($form, event) {
             event.preventDefault();
-            
-            // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
+            var subject = $("input#subject").val();
             var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
+
+            $this = $("#sendMessageButton");
+            $this.prop("disabled", true);
+
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "contact.php",
                 type: "POST",
                 data: {
                     name: name,
                     email: email,
+                    subject: subject,
                     message: message
                 },
                 cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
+                success: function () {
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
+                            .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Pranešimas išsiųstas. </strong>");
+                            .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
+                            .append('</div>');
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
-                    // Fail message
+                error: function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                            .append("</button>");
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
                     $('#contactForm').trigger("reset");
                 },
+                complete: function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false);
+                    }, 1000);
+                }
             });
         },
-        filter: function() {
+        filter: function () {
             return $(this).is(":visible");
         },
     });
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
+    $("a[data-toggle=\"tab\"]").click(function (e) {
         e.preventDefault();
         $(this).tab("show");
     });
 });
 
-// When clicking on Full hide fail/success boxes
-$('#name').focus(function() {
+$('#name').focus(function () {
     $('#success').html('');
 });
