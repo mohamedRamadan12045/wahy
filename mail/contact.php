71c214c1a54727a06c73b391f4e1c41ebc27e8e9
mohamedRamadan12045 <107187838+mohamedRamadan12045@users.noreply.github.com>
2022-06-09 14:33:28 +0200

    Update contact.php
    Signed-off-by: mohamedRamadan12045 <107187838+mohamedRamadan12045@users.noreply.github.com>

--- mail/contact.php
+++ mail/contact.php
@@ -1,20 +1,24 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
-}
-
+// Check for empty fields
+if(empty($_POST['name'])        ||
+   empty($_POST['email'])       ||
+   empty($_POST['message']) ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+    echo "No arguments Provided!";
+    return false;
+   }
+    
 $name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-
-$to = "m.ramadan38.995@gmail.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
-
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+    
+// Create the email and send the message
+$to = 'm.ramadan38.995@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Interneto svetainė:  $name";
+$email_body = "Jūs gavote naują pranešimą iš savo svetainės.\n\n"."Detalės:\n\nVardas: $name\n\nEl. paštas: $email_address\n\nPranešimas:\n$message";
+$headers = "From: m.ramadan38.995@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Atsakyti: $email_address"; 
+mail($to,$email_subject,$email_body,$headers);
+return true;            
 ?>
